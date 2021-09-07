301631dab3fb228f41a970bc0df5a0517041ed94
Sergio Costa <sergiocosta1970@gmail.com>
2021-09-06 22:01:13 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/costa1970" target="_blank" rel="noreferrer">Sergio Costa</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido em 2021 </small></p>
+            </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
